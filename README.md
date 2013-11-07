f4a90e46b743b532a34a847107bac5dc358004b0
durio <durio.thu@gmail.com>
2013-11-07 15:56:45 +0800

    First working version

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 tsinghua-aqjy
 =============
+
+Script for taking an exam on http://aqjy.tsinghua.edu.cn/
+
+Users need to create a `config.py` manually, with contents below:
+
+	EXAM_ID = '<exam id>'
+	LIBRARY_IDS = ['343181', '343732', '53453', '343589', '344072', '53342']
+	USER_ID = '<student id>'
+	USER_PASSWORD = '<student password>'
+
+Replace everything in angle brackets with your own values.
---
+++ exam.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from config import *
+
+import mechanize
+import re
+
+br = mechanize.Browser()
+library = {}
+
+LIBRARY_RE = re.compile(ur'<tr style="border: 0;">\s*<td style="border: 0;">\s*(\d+)&nbsp;&nbsp;\s*(单选题|多选题|判断题)\s*</td>\s*'
+        + ur'<td style="border: 0;"><a title="查看详细" href="/exam/studenttest/studentTestAction!gotoSearchThemeOneDetail\.do\?themeid=(\d+)" target="_blank">(.*?)</a></td>\s*</tr>'
+        + ur'.*?<tr>\s*<td colspan="3"><a id="da_\d+" class="da" href="javascript: getDaan\(\'da_\d+\'\);">正确答案：</a>\s*'
+        + ur'<font id="an_\d+" class="an" color="red" style="display: none;">\s*(\S+)\s*</font>\s*</td>\s*</tr>', re.DOTALL)
+QUESTION_RE = re.compile(ur'题干： (.*?)\(分数：\d+分\)', re.DOTALL)
+
+def scrap_library_page(page):
+    count = 0
+    for nr, qtype, qid, qtext, qans in LIBRARY_RE.findall(page):
+        if qtype == u'单选题':
+            library[qtext] = qans
+        elif qtype == u'多选题':
+            library[qtext] = qans.split(u',')
+        elif qtype == u'判断题' and qans in (u'对', u'错'):
+            library[qtext] = 'A' if qans == u'对' else 'B'
+        else:
+            continue
+        count += 1
+    print count, 'question(s) loaded.'
+
+# Log in
+print 'Initializing'
+br.open('http://aqjy.tsinghua.edu.cn/')
+print 'Logging in 1/2'
+br.select_form(predicate=(lambda form: form.attrs.get('id') == 'loginForm'))
+br['username'] = USER_ID
+br['password'] = USER_PASSWORD
+br.submit()
+
+print 'Logging in 2/2'
+br.select_form(nr=0)
+br.set_all_readonly(False)
+br['j_username'] = USER_ID
+br['j_password'] = 'ticket:null'
+br.submit()
+
+# Load library
+count = 0
+for library_id in LIBRARY_IDS:
+    count += 1
+    print 'Loading library %d/%d: %s' % (count, len(LIBRARY_IDS), library_id)
+    br.open('http://aqjy.tsinghua.edu.cn/exam/studenttest/studentTestAction!gotoSearchTheme.do?knowid='
+            + library_id + '&deptId=0')
+    scrap_library_page(br.response().read().decode('gb18030', 'replace'))
+
+# Answer
+print 'Loading exam 1/3'
+br.open('http://aqjy.tsinghua.edu.cn/exam/studentexame/studentExameAction!getExame.do')
+print 'Loading exam 2/3'
+br.open('http://aqjy.tsinghua.edu.cn/exam/studentexame/studentExameAction!beforeGotoExame.do?exameid=' + EXAM_ID)
+questions = max(map(int, re.findall(ur'javascript:formsubmit\(\'(\d+)\'\)', br.response().read().decode('gb18030', 'replace'))))
+
+print 'Loading exam 3/3'
+br.select_form('exameform')
+br.set_all_readonly(False)
+br['pageNum'] = '1'
+br.submit()
+
+answer_nr = 1
+while answer_nr <= questions:
+    question = QUESTION_RE.search(br.response().read().decode('gb18030', 'replace')).group(1)
+    print 'Answering %d/%d: %s' % (answer_nr, questions, question)
+    answer = library[question]
+    br.select_form('exameform')
+    if isinstance(answer, basestring):
+        br['answerradio'] = [answer]
+    else:
+        br['answercheckbox'] = answer
+    br.set_all_readonly(False)
+    br['pageNum'] = str(answer_nr + 1)
+    answer_nr += 1
+    br.submit()
+
+print 'Finished'
